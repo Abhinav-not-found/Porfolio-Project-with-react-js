0f3973bdd100d4ec0390fbe4203c9612080a1d7a
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-06-27 22:22:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
-// import Contact from './components/Contacts/Contact';
+import { Routes,Route } from 'react-router-dom';
+import Contact from './components/Contacts/Contact';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      
-      {/* <Contact></Contact> */}
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element = {<Home></Home>}/>
+        <Route path='/About' element={<h1>Underdevelopment</h1>}/>
+        <Route path='/Contact' element = {<Contact></Contact>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from "./Subtract.png"
 import "./Navbar.css"
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
         <nav>
          <img src={logo}></img>
           <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Contact</a></li>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/About'}>About</Link></li>
+            <li><Link to={'/Contact'}>Contact</Link></li>
           </ul>
         </nav>
     </div>
